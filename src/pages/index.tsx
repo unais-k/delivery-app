c893399765e40ea56cc15f0149bec82d4fd6a418
unais-k <unaissafa008@gmail.com>
2023-12-08 20:10:35 +0530

    some other errors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import CustomerService from "@/components/modules/home/CustomerService";
 import LandingSection from "@/components/modules/home/LandingSection";
 import React from "react";
@@ -12,12 +11,6 @@ const HOME = () => {
             <CustomerService />
         </div>
     );
-=======
-import React from "react";
-
-const HOME = () => {
-    return <div>HOME</div>;
->>>>>>> cff0a9e8e0c71b10a6adf87787b5fb974fdadb4e
 };
 
 export default HOME;
